046382e32f115244e3913ffa31fb3d83c26e5f4e
Sasikumar <sasikumar.arumugam@technotackle.com>
2025-02-26 18:11:34 +0530

    Update .gitignore to exclude node_modules, .env, config.json

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,5 +4,8 @@ const userController=require("../controllers/user.contoller");
 const configureMulter=require("../utils/fileuUploads")
 
 route.post("/create",configureMulter({ allowMultipleFiles: true }),userController.createUser.handler);
+route.post("/login",userController.userLogin.handler);
+route.post("/forget-password",userController.forgotPassword.handler);
+route.post("/reset-password",userController.resetPassword.handler);
 
 module.exports=route;
